344756d69326cb9e6ddce83c9bb3434224754df7
yunus-EmreX <138483345+yunus-EmreX@users.noreply.github.com>
2026-09-14 01:28:01 +0300

    Delete ReconCAS directory

--- ReconCAS/gui/theme.py
+++
@@ -1,8 +0,0 @@
-# Siberpunk & Endüstriyel Tema Entegrasyonu
-THEME = {
-    "bg": "#050505", "fg": "#00FF41", "btn_bg": "#111111", 
-    "active": "#008F11", "accent": "#00FFFF", "grid": "#333333",
-    "font_main": ("Consolas", 10, "bold"), "font_title": ("Consolas", 11, "bold"),
-    "error": "#FF003C", "warning": "#FFB300"
-}
-
